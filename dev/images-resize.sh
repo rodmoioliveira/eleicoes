395c7109e94527d2a92ad3433be8eb78dad18d82
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2024-09-25 08:47:16 -0300

    feat: enhance mobile layout
    Signed-off-by: Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>

--- dev/images-resize.sh
+++ dev/images-resize.sh
@@ -15,14 +15,27 @@ resize-192() {
     xargs -n2 -P10 convert -quality 100 -define webp:method=6 -resize 192x
 }
 
+resize-300() {
+  printf "Resizing to 300px ...\n"
+  paste -d ' ' \
+    <(find images/original -type f) \
+    <(find images/original -type f | sed -e 's@images/original/@@g' | awk -F'.' '{print "images/300/"$1".webp"}') |
+    xargs -n2 -P10 convert -quality 100 -define webp:method=6 -resize 300x
+}
+
 resize-assert() {
   diff \
     <(find images/original -type f | awk -F'/' '{print $NF}' | awk -F'.' '{print $1}' | sort) \
     <(find images/192 -type f | awk -F'/' '{print $NF}' | awk -F'.' '{print $1}' | sort)
+
+  diff \
+    <(find images/original -type f | awk -F'/' '{print $NF}' | awk -F'.' '{print $1}' | sort) \
+    <(find images/300 -type f | awk -F'/' '{print $NF}' | awk -F'.' '{print $1}' | sort)
 }
 
 main() {
   resize-192
+  resize-300
   resize-assert
 }
 
